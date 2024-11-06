456f0a7a73fa21d5c7905e87b5fe9b60b4eaa588
Taesongsong <aksmf1533@gmail.com>
2024-11-06 12:48:04 +0900

    1106

---
+++ 1106/js/script(jq).js
@@ -0,0 +1,15 @@
+$('.sub').hide();
+$('.sub').eq(0).show();
+
+$('.m_menu>li>a').click(function(){
+    var kkk = $(this).next('.sub').css('display');
+    
+    if(kkk == "none"){
+        $('.sub').slideUp();
+        $(this).next('.sub').slideDown();
+    }else{
+        $('.sub').slideUp();
+    }
+
+
+});
